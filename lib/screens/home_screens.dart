233de87944b21bbf0fa5061016f6330c169d7e7b
IshaqFarid280 <IshaqFarid280>
2024-07-12 21:51:29 +0500

    sdfsdf

--- lib/screens/home_screens.dart
+++ lib/screens/home_screens.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:http/http.dart' as http;
+import 'package:image_picker/image_picker.dart';
 import 'dart:convert';
 import 'package:intl/intl.dart';
 import 'package:mmfacebooookapp/reusable/customIcons.dart';
@@ -153,6 +156,178 @@ class _DashboardScreenState extends State<DashboardScreen> {
     }
   }
 
+  void _showPostDialog(BuildContext context) {
+    final TextEditingController _postController = TextEditingController();
+    File? _imageFile;
+
+    Future<void> _pickImage() async {
+      final pickedFile = await ImagePicker().pickImage(source: ImageSource.gallery);
+      if (pickedFile != null) {
+        setState(() {
+          _imageFile = File(pickedFile.path);
+        });
+      }
+    }
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return StatefulBuilder(
+          builder: (context, setState) {
+            return AlertDialog(
+              backgroundColor: primaryTextColor,
+              title: largeText(title: 'Create Post'),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  _imageFile != null
+                      ? Center(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        color: Colors.grey,
+                      ),
+                      child: Image.file(
+                        _imageFile!,
+                        height: 150,
+                        width: 240,
+                        fit: BoxFit.cover, // Ensure the image covers the container
+                      ),
+                    ),
+                  )
+                      : InkWell(
+                    onTap: () {
+                      _pickImage().then((_) {
+                        // Update state after picking image
+                        setState(() {});
+                      });
+                    },
+                    child: Center(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          color: Colors.grey,
+                        ),
+                        width: 240,
+                        height: 150,
+                        child: Icon(
+                          Icons.add,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  Padding(
+                    padding: const EdgeInsets.only(top: 8.0),
+                    child: TextField(
+                      controller: _postController,
+                      style: TextStyle(color: Colors.white), // Set text color to white
+                      decoration: InputDecoration(
+                        hintText: 'Enter your post content',
+                        hintStyle: TextStyle(color: Colors.white.withOpacity(0.7)), // Adjust hint color and opacity
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey),
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                      ),
+                    ),
+                  ),
+
+
+                  // SizedBox(height: 10),
+                  // _imageFile != null
+                  //     ? Image.file(_imageFile!, height: 150)
+                  //     : SizedBox(),
+                  SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      InkWell(
+                        onTap: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8),
+                            color: Colors.transparent,
+                            border: Border.all(color: Colors.grey),
+                          ),
+                          width: MediaQuery.of(context).size.width * 0.25,
+                          height: MediaQuery.of(context).size.height * 0.06,
+                          child: Center(
+                            child: largeText(
+                              title: 'Cancel',
+                              fontSize: 14.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                      InkWell(
+                        onTap: () {
+                          _createPost(_postController.text, _imageFile);
+                          Navigator.of(context).pop();
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8),
+                            color: Colors.blue,
+                            border: Border.all(color: Colors.grey),
+                          ),
+                          width: MediaQuery.of(context).size.width * 0.25,
+                          height: MediaQuery.of(context).size.height * 0.06,
+                          child: Center(
+                            child: largeText(
+                              title: 'Post',
+                              fontSize: 14.0,
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+
+  Future<void> _createPost(String content, File? imageFile) async {
+    if (_userPages != null && _userPages!.isNotEmpty) {
+      final page = _userPages![
+          0]; // You can change this to allow selecting different pages.
+      final pageAccessToken = await _getPageAccessToken(page['id']);
+      if (pageAccessToken != null) {
+        final Uri postUrl =
+            Uri.parse('https://graph.facebook.com/v20.0/${page['id']}/photos');
+
+        var request = http.MultipartRequest('POST', postUrl);
+        request.headers['Authorization'] = 'Bearer $pageAccessToken';
+
+        // Add message field
+        request.fields['message'] = content;
+
+        // Add image file if selected
+        if (imageFile != null) {
+          request.files
+              .add(await http.MultipartFile.fromPath('source', imageFile.path));
+        }
+
+        var response = await request.send();
+        if (response.statusCode == 200) {
+          print('Post created successfully');
+          _fetchUserPages(); // Refresh the feed to show the new post.
+        } else {
+          print('Failed to create post: ${response.reasonPhrase}');
+        }
+      }
+    }
+  }
+
   String _formatDateTime(String dateTimeStr) {
     final dateTime = DateTime.parse(dateTimeStr);
     return DateFormat('yMMMd').add_jm().format(dateTime);
@@ -167,7 +342,11 @@ class _DashboardScreenState extends State<DashboardScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: CustomFloatingAction(onTap: () {  },),
+      floatingActionButton: CustomFloatingAction(
+        onTap: () {
+          _showPostDialog(context);
+        },
+      ),
       appBar: AppBar(
         title: largeText(title: 'Dashboard'),
       ),
@@ -191,8 +370,10 @@ class _DashboardScreenState extends State<DashboardScreen> {
                   }
 
                   final post = _allPageFeeds[index];
-                  final commentsCount = post['comments']?['summary']?['total_count'] ?? 0;
-                  final likesCount = post['likes']?['summary']?['total_count'] ?? 0;
+                  final commentsCount =
+                      post['comments']?['summary']?['total_count'] ?? 0;
+                  final likesCount =
+                      post['likes']?['summary']?['total_count'] ?? 0;
                   final sharesCount = post['shares']?['count'] ?? 0;
                   return Card(
                     color: secondaryTextColor,
@@ -207,7 +388,7 @@ class _DashboardScreenState extends State<DashboardScreen> {
                         children: [
                           post['full_picture'] != null
                               ? Container(
-                            clipBehavior: Clip.antiAlias,
+                                  clipBehavior: Clip.antiAlias,
                                   decoration: BoxDecoration(
                                     borderRadius: BorderRadius.circular(10),
                                     image: DecorationImage(
@@ -216,8 +397,10 @@ class _DashboardScreenState extends State<DashboardScreen> {
                                         ),
                                         fit: BoxFit.contain),
                                   ),
-                            child: Image.network(post['full_picture'],),
-                          )
+                                  child: Image.network(
+                                    post['full_picture'],
+                                  ),
+                                )
                               : const Center(
                                   child: Icon(
                                     Icons.error_outline,
@@ -230,11 +413,16 @@ class _DashboardScreenState extends State<DashboardScreen> {
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             crossAxisAlignment: CrossAxisAlignment.end,
                             children: [
-                              Expanded(child: smallText(title :post['message'] ?? 'No message')),
-                              smallText(title:_formatDateTime(post['created_time'])),
+                              Expanded(
+                                  child: smallText(
+                                      title: post['message'] ?? 'No message')),
+                              smallText(
+                                  title: _formatDateTime(post['created_time'])),
                             ],
                           ),
-                          Divider(color: listTileLeadingColor,),
+                          Divider(
+                            color: listTileLeadingColor,
+                          ),
                           Padding(
                             padding: const EdgeInsets.symmetric(horizontal: 10),
                             child: Row(
@@ -244,15 +432,20 @@ class _DashboardScreenState extends State<DashboardScreen> {
                                   children: [
                                     CustomIcon(iconData: Icons.thumb_up),
                                     Sized(),
-                                    smallText(title: likesCount.toString(),fontSize: 16.0),
+                                    smallText(
+                                        title: likesCount.toString(),
+                                        fontSize: 16.0),
                                     Sized(),
                                   ],
                                 ),
                                 Row(
                                   children: [
-                                    CustomIcon(iconData: Icons.messenger_outlined),
+                                    CustomIcon(
+                                        iconData: Icons.messenger_outlined),
                                     Sized(),
-                                    smallText(title: commentsCount.toString(),fontSize: 16.0),
+                                    smallText(
+                                        title: commentsCount.toString(),
+                                        fontSize: 16.0),
                                     Sized(),
                                   ],
                                 ),
@@ -260,7 +453,9 @@ class _DashboardScreenState extends State<DashboardScreen> {
                                   children: [
                                     CustomIcon(iconData: Icons.share_sharp),
                                     Sized(),
-                                    smallText(title: sharesCount.toString(),fontSize: 16.0),
+                                    smallText(
+                                        title: sharesCount.toString(),
+                                        fontSize: 16.0),
                                   ],
                                 ),
                               ],
